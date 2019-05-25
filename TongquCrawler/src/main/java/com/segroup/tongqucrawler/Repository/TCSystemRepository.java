a85de31653fb6402a40a3e77e9ba5ec763018afe
Dai Fangyue <dfyshisb@163.com>
2019-05-25 15:06:11 +0800

    Finish Crawling & Database Access

---
+++ TongquCrawler/src/main/java/com/segroup/tongqucrawler/Repository/TCSystemRepository.java
@@ -0,0 +1,7 @@
+package com.segroup.tongqucrawler.Repository;
+
+import com.segroup.tongqucrawler.Entity.TCSystem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TCSystemRepository extends JpaRepository<TCSystem, String> {
+}
--- TongquCrawler/src/main/java/com/segroup/tongqucrawler/crawler/TongquCrawler.java
+++
@@ -1,7 +0,0 @@
-public class TongquCrawler {
-    public static void main(String args[]) {
-        RequestSender rs = new RequestSender();
-        String response = rs.send("https://tongqu.me/act/20350", "GET");
-        System.out.println(response);
-    }
-}
