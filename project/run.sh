d92d803da4661d8860c7ee095996b234e3331d75
zhaoshenglong <G245078728@sjtu.edu.cn>
2019-06-02 23:47:29 +0800

    change run.sh to run in host network

--- project/run.sh
+++ project/run.sh
@@ -11,11 +11,11 @@ fi
 
 # Run on demand
 
-docker run -p 9000:9000 -d --name=eureka-server \
+docker run -p 9000:9000 -d --name=eureka-server --net=host\
 	summer855/service-discovery
-docker run -p 9100:9100 -d --name=auth-server \
-	--link=eureka-server summer855/auth-server
+docker run -p 9100:9100 -d --name=auth-server --net=host\
+	summer855/auth-server
 docker run -p 9200:9200 -d --name=tongqu-parser --net=host \
-	--link=eureka-server --link=auth-server summer855/parser
-docker run -p 10000:10000 -d --name=api-gateway \
-	--link=eureka-server summer855/gateway
+	summer855/parser
+docker run -p 10000:10000 -d --name=api-gateway --net=host \
+	summer855/gateway
