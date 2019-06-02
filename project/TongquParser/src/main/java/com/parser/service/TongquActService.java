8871a2c47a66b045caa3ac2378b15a7fe60feee5
zhaoshenglong <G245078728@sjtu.edu.cn>
2019-06-03 00:21:59 +0800

    change feign with hard code url

--- project/TongquParser/src/main/java/com/parser/service/TongquActService.java
+++ project/TongquParser/src/main/java/com/parser/service/TongquActService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("TONGQU-CRAWLER")
+@FeignClient(name = "crawler", url = "http://47.100.50.175:8848/")
 public interface TongquActService {
     @RequestMapping(value = "/getact", method = RequestMethod.GET)
     JSONArray getAllActs();
