c0449f16e81788cc54c8947227e7ccb66b3b111e
zhaoshenglong <G245078728@sjtu.edu.cn>
2019-06-02 17:58:32 +0800

    fix feign version bug

--- project/TongquParser/src/main/java/com/parser/controller/ParseController.java
+++ project/TongquParser/src/main/java/com/parser/controller/ParseController.java
@@ -1,15 +1,9 @@
 package com.parser.controller;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.huaban.analysis.jieba.JiebaSegmenter;
-import com.huaban.analysis.jieba.SegToken;
 import com.huaban.analysis.jieba.WordDictionary;
-import com.netflix.discovery.converters.Auto;
 import com.parser.service.TongquActService;
 import com.parser.service.TongquService;
-import com.parser.util.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +14,6 @@ import org.springframework.web.client.RestTemplate;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Iterator;
-import java.util.List;
 
 
 @RestController
@@ -32,13 +24,17 @@ public class ParseController {
     @Autowired
     private TongquActService tongquActService;
 
+
     @Autowired
     private TongquService tongquService;
+
 
     public ParseController(){
         Path path = Paths.get(new File( getClass().getClassLoader().getResource("static/jieba.dict").getPath() ).getAbsolutePath() ) ;
         WordDictionary.getInstance().loadUserDict( path ) ;
     }
+
+
 
     @RequestMapping("/act/test")
     public JSONArray Parse(@RequestParam(required = false,defaultValue = "满分素拓")String text){
@@ -49,6 +45,7 @@ public class ParseController {
     JSONArray getAllAct () {
         return tongquActService.getAllActs();
     }
+
 
 }
 
