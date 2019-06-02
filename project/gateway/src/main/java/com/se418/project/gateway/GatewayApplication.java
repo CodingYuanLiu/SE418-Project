b8c625afa12ee9adc05e310f50dbd6b04e3b7a1a
zhaoshenglong <G245078728@sjtu.edu.cn>
2019-06-02 20:07:05 +0800

    add cors configuration

--- project/gateway/src/main/java/com/se418/project/gateway/GatewayApplication.java
+++ project/gateway/src/main/java/com/se418/project/gateway/GatewayApplication.java
@@ -4,11 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class GatewayApplication {
 
+    @Bean
+    protected WebMvcConfigurer corsConfigure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
--- project/run.sh
+++ project/run.sh
@@ -15,7 +15,7 @@ docker run -p 9000:9000 -d --name=eureka-server \
 	summer855/service-discovery
 docker run -p 9100:9100 -d --name=auth-server \
 	--link=eureka-server summer855/auth-server
-docker run -p 9200:9200 -d --name=tongqu-parser \
+docker run -p 9200:9200 -d --name=tongqu-parser --net=host \
 	--link=eureka-server --link=auth-server summer855/parser
 docker run -p 10000:10000 -d --name=api-gateway \
 	--link=eureka-server summer855/gateway
