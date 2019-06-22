9cc683cec5823795cd672eca08b9165db0769c63
zhaoshenglong <G245078728@sjtu.edu.cn>
2019-06-22 16:42:43 +0800

    double the service provider

--- project/TongquParser/src/main/java/com/parser/service/TongquActService.java
+++ project/TongquParser/src/main/java/com/parser/service/TongquActService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "crawler", url = "http://47.100.50.175:8848/")
+@FeignClient("TONGQU-CRAWLER")
 public interface TongquActService {
     @RequestMapping(value = "/getact", method = RequestMethod.GET)
     String getAllActs();
